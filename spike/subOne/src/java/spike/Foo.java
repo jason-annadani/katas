25b874ee4cdda2ae80a99ae796ea06390a5a0e5c
Jason Annadani <jason.annadani@gmail.com>
2014-11-25 09:54:47 +0000

    staring point

---
+++ spike/subOne/src/java/spike/Foo.java
@@ -0,0 +1,13 @@
+package spike;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+public class Foo {
+	public static void main(String[] args) {
+		List<String> list = Lists.newArrayList("Foo", "Goo");
+		System.out.println(list);
+	}
+	
+}
---
+++ spike/subTwo/src/java/spike/Bar.java
@@ -0,0 +1,12 @@
+package spike;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+public class Bar {
+	public static void main(String[] args) {
+		List<String> list = Lists.newArrayList("Bar", "Wah");
+		System.out.println(list);
+	}
+}
